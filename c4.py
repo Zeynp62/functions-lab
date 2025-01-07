e823eeb10896edf9967f0b0cf9ebadb3fffc6829
Zeynp62 <zainab.fadhel.alaali62@gmail.com>
2025-01-07 22:17:00 +0300

    4 done

--- c4.py
+++ c4.py
@@ -1,1 +1,9 @@
 #Write a function named product that takes an arbitrary number of numbers, multiplies them all together, and returns the product.(HINT: Review your notes on *args).
+
+def product(*args):
+  product = 1
+  for arg in args:
+    product *= arg
+  return product
+
+print(product(10,5,10,2))
